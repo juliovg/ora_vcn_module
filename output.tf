b6e7dca3f92d864d14795028768abea625bb96a4
julio vasquez <julio.v.g@gmail.com>
2022-03-28 15:06:22 +0200

    update

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "vcn_name" {
   description = "The state of the VCN."
-  value       = oci_core_vcn.test_vcn.display_name
+  value       = oci_core_vcn.poc_vcn.display_name
 }
 
 output "vcn_state" {
   description = "The state of the VCN."
-  value       = oci_core_vcn.test_vcn.state
+  value       = oci_core_vcn.poc_vcn.state
 }
 
 output "vcn_id" {
   description = "The state of the VCN."
-  value       = oci_core_vcn.test_vcn.id
+  value       = oci_core_vcn.poc_vcn.id
 }
