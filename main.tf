7361b94a9ca9be3c3122a2661aafd3fd304ae1a8
julio vasquez <julio.v.g@gmail.com>
2022-03-29 09:44:59 +0200

    update

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "oci_core_vcn" "poc_vcn" {
 resource "oci_core_subnet" "these" {
   for_each                   = { for subnet in local.subnets : "${subnet.vcn_name}.${subnet.subnet_key}" => subnet }
     display_name             = each.value.display_name
-    vcn_id                   = oci_core_vcn.these[each.value.vcn_name].id
+    vcn_id                   = oci_core_vcn.poc_vcn[each.value.vcn_name].id
     cidr_block               = each.value.cidr
     compartment_id           = each.value.compartment_id
 }
